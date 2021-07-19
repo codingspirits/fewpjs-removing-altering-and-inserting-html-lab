34a40c74942ea95341e313aeccc5e330a466df0d
codingspirits <vincci.cheng.9@gmail.com>
2021-07-19 22:44:11 +1000

    completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+document.querySelector('main').remove()
+
+body = document.getElementsByTagName('body')
+newHeader = document.createElement('h1')
+document.body.appendChild(newHeader)
+newHeader.id = 'victory'
+newHeader.className = 'victory'
+newHeader.innerHTML = 'Some interesting text'
